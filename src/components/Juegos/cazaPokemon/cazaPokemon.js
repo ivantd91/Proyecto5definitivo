4f63bfa918a5323bc414e8dac9845ffe9815e705
Iván <ivan_oia91@hotmail.com>
2025-07-23 23:47:02 +0200

    Corrección del bug al hacer clic en Pokémon (removeChild aplicado correctamente)

--- src/components/Juegos/cazaPokemon/cazaPokemon.js
+++ src/components/Juegos/cazaPokemon/cazaPokemon.js
@@ -28,10 +28,11 @@ export function juegoCazaPokemon() {
   recordEl.textContent = 'Récord: ' + record
 
   let contador = 0
+
   function obtenerVelocidadActual() {
-    if (contador > 20) return 200
-    if (contador > 10) return 300
-    if (contador > 5) return 400
+    if (contador > 40) return 200
+    if (contador > 25) return 300
+    if (contador > 10) return 400
     return 500
   }
   const contadorEl = document.createElement('div')
@@ -84,6 +85,13 @@ export function juegoCazaPokemon() {
   lanzarPokemon()
 
   function atrapar(pokeElemento) {
+    if (!campo.contains(pokeElemento)) return
+
+    campo.removeChild(pokeElemento)
+    vivos--
+    contador++
+    contadorEl.textContent = 'capturas: ' + contador
+
     const bola = document.createElement('img')
     bola.src = ultraBallImg
     bola.className = 'pokeball'
@@ -92,26 +100,15 @@ export function juegoCazaPokemon() {
 
     const pokeRect = pokeElemento.getBoundingClientRect()
     const campoRect = campo.getBoundingClientRect()
-    const width = pokeRect.width + 'px'
-    const height = pokeRect.height + 'px'
-    const left = pokeRect.left - campoRect.left + 'px'
-    const top = pokeRect.top - campoRect.top + 'px'
-
-    bola.style.width = width
-    bola.style.height = height
-    bola.style.left = left
-    bola.style.top = top
+    bola.style.width = pokeRect.width + 'px'
+    bola.style.height = pokeRect.height + 'px'
+    bola.style.left = pokeRect.left - campoRect.left + 'px'
+    bola.style.top = pokeRect.top - campoRect.top + 'px'
 
     campo.appendChild(bola)
 
     setTimeout(() => {
       if (campo.contains(bola)) campo.removeChild(bola)
-      if (campo.contains(pokeElemento)) {
-        campo.removeChild(pokeElemento)
-        vivos--
-        contador++
-        contadorEl.textContent = 'Capturas: ' + contador
-      }
     }, 300)
   }
   function terminarJuego() {

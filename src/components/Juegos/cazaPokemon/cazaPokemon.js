38175b8ace7f35d2b1125e8ddf3095bba0669133
Iván <ivan_oia91@hotmail.com>
2025-07-23 23:55:25 +0200

    Corrección del bug al hacer clic en Pokémon (removeChild aplicado correctamente2)

--- src/components/Juegos/cazaPokemon/cazaPokemon.js
+++ src/components/Juegos/cazaPokemon/cazaPokemon.js
@@ -85,13 +85,6 @@ export function juegoCazaPokemon() {
   lanzarPokemon()
 
   function atrapar(pokeElemento) {
-    if (!campo.contains(pokeElemento)) return
-
-    campo.removeChild(pokeElemento)
-    vivos--
-    contador++
-    contadorEl.textContent = 'capturas: ' + contador
-
     const bola = document.createElement('img')
     bola.src = ultraBallImg
     bola.className = 'pokeball'
@@ -100,15 +93,26 @@ export function juegoCazaPokemon() {
 
     const pokeRect = pokeElemento.getBoundingClientRect()
     const campoRect = campo.getBoundingClientRect()
-    bola.style.width = pokeRect.width + 'px'
-    bola.style.height = pokeRect.height + 'px'
-    bola.style.left = pokeRect.left - campoRect.left + 'px'
-    bola.style.top = pokeRect.top - campoRect.top + 'px'
+    const width = pokeRect.width + 'px'
+    const height = pokeRect.height + 'px'
+    const left = pokeRect.left - campoRect.left + 'px'
+    const top = pokeRect.top - campoRect.top + 'px'
+
+    bola.style.width = width
+    bola.style.height = height
+    bola.style.left = left
+    bola.style.top = top
 
     campo.appendChild(bola)
 
     setTimeout(() => {
       if (campo.contains(bola)) campo.removeChild(bola)
+      if (campo.contains(pokeElemento)) {
+        campo.removeChild(pokeElemento)
+        vivos--
+        contador++
+        contadorEl.textContent = 'Capturas: ' + contador
+      }
     }, 300)
   }
   function terminarJuego() {
